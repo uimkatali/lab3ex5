6f34084c30050682f87f1e7bc015e5e528b125b1
uimkatali <b8bf6b5@gmail.com>
2023-10-02 19:03:20 +0300

    Add project files.

---
+++ lab3ex5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sir = Console.ReadLine();
+            int nrVocale = 0;
+            for (int i = 0; i < sir.Length; i++)
+            {
+               if(sir[i] == 'a' || sir[i] == 'e' || sir[i] == 'i' || sir[i] == 'o' || sir[i] == 'u')
+                {
+                    nrVocale++;
+                }
+            }
+            Console.WriteLine(nrVocale);
+        }
+    }
+}
